49d3b7c2d046804f4fcf0e54c85ebfdc0c6bdb87
devhak <devhak.enzo@gmail.com>
2024-03-19 23:06:32 +0900

    완전탐색-퇴사 문제풀이

--- src/백준/boj14501_퇴사.java
+++ src/백준/boj14501_퇴사.java
@@ -12,23 +12,42 @@ import java.util.StringTokenizer;
  */
 public class boj14501_퇴사 {
 
+    private static int N;
     static int maxCnt = Integer.MIN_VALUE;
-    static int [][]work ;
+    static int[][] work;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        work = new int [N][2];
-        for(int i = 0 ;i<N ;i++){
+        N = Integer.parseInt(st.nextToken());
+        work = new int[N][2];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            for(int j = 0; j<2; j++){
+            for (int j = 0; j < 2; j++) {
                 work[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         //완전 탐색
-
-
+        dfs(0, 0);
+        System.out.println(maxCnt);
     }
+
+    private static void dfs(int day, int pay) {
+        //기저조건
+        if (day >= N) {
+            maxCnt = Math.max(pay, maxCnt);
+            return;
+        }
+
+        //다음 상담
+        if (day + work[day][0] <= N) { // 상담을 끝마칠 수 있다면 -> 상담이 끝난 날짜와 상담비 넣음
+            dfs(day + work[day][0], pay + work[day][1]);
+        } else { // 상담을 끝마칠 수 없다면 -> 상담이 끝난 날짜만 넘겨준다(탈출 조건으로 써먹음)
+            dfs(day + work[day][0], pay);
+        }
+
+        dfs(day + 1, pay);
+    }
+
 }
